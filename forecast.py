8237a845964c480e6e87ac60ac9449095e6a4dec
MeaMoon <vsamajev@gmail.com>
2021-04-04 11:37:27 +0300

    initial commit

--- forecast.py
+++ forecast.py
@@ -26,14 +26,14 @@ def getWeather(canvas):
 
 canvas = tk.Tk()
 #canvas.geometry("600x500")
-canvas.title("Weather App")
+#canvas.title("Weather App")
 f = ("poppins", 15, "bold")
 t = ("poppins", 35, "bold")
 
 textField = tk.Entry(canvas, justify='center', width = 20, font = t)
-textField.pack(pady = 20)
-textField.focus()
-textField.bind('<Return>', getWeather)
+#textField.pack(pady = 20)
+#textField.focus()
+#textField.bind('<Return>', getWeather)
 
 label1 = tk.Label(canvas, font=t)
 label1.pack()
